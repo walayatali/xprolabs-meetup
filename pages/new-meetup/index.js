174aa8ce347e57b43a9cd4d724b21637a356799c
walayatali <walayat.ali@xprolabs.com>
2023-02-06 14:59:33 +0500

    Added API to add meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router'
 
 function NewMeetUp(props)	{
+	const router = useRouter()
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-	const addMeetupHandler = (data) => {
-		console.log(data)
-	}
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
+  }
 
 	return (
 		<NewMeetupForm onAddMeetup={addMeetupHandler}/>
