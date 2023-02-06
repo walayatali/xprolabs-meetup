381f6ed1c0cd38fd726af2e1b9807b63ef949201
walayatali <walayat.ali@xprolabs.com>
2023-02-06 14:08:31 +0500

    add static rendering for specific meetup by Id

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
@@ -9,6 +9,39 @@ function MeetupDetails() {
       description='This is a first meetup'
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetUpId: 'mx1',
+        },
+      },
+      {
+        params: {
+          meetUpId: 'mx2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetUpId = context.params.meetUpId
+  return {
+    props: {
+      meetupData: {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetUpId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      }
+    }
+  }
 }
 
 export default MeetupDetails;
